291287f35a1e79b72a5623ac65633e480a23b66e
Jrmuys <jrmuyskens@gmail.com>
2020-08-21 12:11:31 -0400

    adding paypal integration

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -9,7 +9,18 @@ import {
   OnChanges,
   SimpleChanges,
   Input,
+  ViewChild,
+  ElementRef,
 } from '@angular/core';
+
+interface PayPalItem {
+  description: string;
+  amount: {
+    currency_code: 'USD';
+    value: number;
+  };
+}
+declare var paypal: any;
 
 @Component({
   selector: 'app-cart',
@@ -17,6 +28,7 @@ import {
   styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit, OnDestroy {
+  @ViewChild('paypal', { static: true }) paypalElement: ElementRef;
   constructor(
     private cartService: CartService,
     private authService: AuthService
@@ -35,7 +47,46 @@ export class CartComponent implements OnInit, OnDestroy {
     'delete',
   ];
 
+  paypalGetItems(): PayPalItem[] {
+    let paypalItems: PayPalItem[];
+    this.cart.forEach((cartItem) => {
+      let newPayPalItem: PayPalItem = {
+        description: cartItem.title,
+        amount: {
+          currency_code: 'USD',
+          value: cartItem.itemTotal,
+        },
+      };
+    });
+
+    return;
+  }
+
   ngOnInit(): void {
+    this.loadExternalScript(
+      'https://www.paypal.com/sdk/js?client-id=ASo29H6AXV4zhdJiJBNDjxp1VXECNJF2CFApAIloYin79pP9HjbHH85AS1adEcLy9hrEtEf4Qu5sORkL'
+    ).then(() => {
+      paypal
+        .Buttons({
+          createOrder: (data, actions) => {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  description: 'test',
+                  amount: {
+                    currency_code: 'USD',
+                    value: 20,
+                  },
+                },
+              ],
+            });
+          },
+          onApprove: async (data, actions) => {
+            const order = await actions.order.capture();
+          },
+        })
+        .render(this.paypalElement.nativeElement);
+    });
     this.cartService.getCart();
     this.cartSub = this.cartService
       .getCartUpdateListener()
@@ -82,5 +133,14 @@ export class CartComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.cartSub.unsubscribe();
+  }
+
+  private loadExternalScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = scriptUrl;
+      scriptElement.onload = resolve;
+      document.body.appendChild(scriptElement);
+    });
   }
 }
