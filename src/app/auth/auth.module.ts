1afbf865e57a5302cd0cdeee4cec0a5a7b81a0e3
Joel Muyskens <jrmuyskens@gmail.com>
2021-03-01 22:58:33 -0500

    Add verify prompt to module, reset package.json

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -8,9 +8,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RequestResetComponent } from './request-reset/request-reset.component';
 import { ResetComponent } from './reset/reset.component';
+import { VerifyPromptComponent } from './verify-prompt/verify-prompt.component';
 
 @NgModule({
-  declarations: [LoginComponent, SignupComponent, VerifyComponent, RequestResetComponent, ResetComponent],
+  declarations: [LoginComponent, SignupComponent, VerifyComponent, RequestResetComponent, ResetComponent, VerifyPromptComponent],
   imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}
