9f11e1342700ef42c77e9c99eff050189f708505
Joel Muyskens <jrmuyskens@gmail.com>
2021-03-01 22:57:11 -0500

    Added verify prompt

--- src/app/auth/verify/verify.component.ts
+++ src/app/auth/verify/verify.component.ts
@@ -12,11 +12,39 @@ import { ActivatedRoute } from '@angular/router';
 export class VerifyComponent implements OnInit {
   isLoading = false;
   error: Error;
+  validForm: Boolean = false;
+  sent: Boolean = false;
 
   constructor(
     public authService: AuthService,
     private router: ActivatedRoute
   ) {}
+
+  /**
+   * Submits the email to authservice to resent the verification email
+   * @param {NgForm} form
+   */
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      this.validForm = false;
+      return;
+    }
+    console.log('Submitting,', form.value.email);
+    this.authService.resendVerification(form.value.email).subscribe(
+      (result) => {
+        this.isLoading = false;
+        console.log('Result: ', result);
+        this.sent = true;
+        this.error = null;
+        form.reset();
+      },
+      (error) => {
+        this.isLoading = false;
+        this.error = error;
+        console.log('error found,', error);
+      }
+    );
+  }
 
   ngOnInit(): void {
     this.isLoading = true;
